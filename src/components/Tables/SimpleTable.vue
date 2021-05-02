73ca6a4b3dd52f79fd53731052c79c0d18a55d87
Roger Rezende <javielrezende@gmail.com>
2021-05-02 16:41:54 -0300

    :sparkles: Component to lists
    Component to lists

--- src/components/Tables/SimpleTable.vue
+++ src/components/Tables/SimpleTable.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <md-table v-model="users" :table-header-color="tableHeaderColor">
+    <md-table v-model="data" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
-        <md-table-cell md-label="Country">{{ item.country }}</md-table-cell>
-        <md-table-cell md-label="City">{{ item.city }}</md-table-cell>
-        <md-table-cell md-label="Salary">{{ item.salary }}</md-table-cell>
+        <md-table-cell
+          v-for="column in columns"
+          :md-label="column.label"
+          :key="column.label"
+          >{{ item[column.name] }}</md-table-cell
+        >
       </md-table-row>
     </md-table>
   </div>
@@ -17,51 +19,19 @@ export default {
   props: {
     tableHeaderColor: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
+    data: {
+      type: Array,
+    },
+    columns: {
+      type: Array,
+    },
   },
   data() {
     return {
       selected: [],
-      users: [
-        {
-          name: "Dakota Rice",
-          salary: "$36,738",
-          country: "Niger",
-          city: "Oud-Turnhout"
-        },
-        {
-          name: "Minerva Hooper",
-          salary: "$23,738",
-          country: "Curaçao",
-          city: "Sinaai-Waas"
-        },
-        {
-          name: "Sage Rodriguez",
-          salary: "$56,142",
-          country: "Netherlands",
-          city: "Overland Park"
-        },
-        {
-          name: "Philip Chaney",
-          salary: "$38,735",
-          country: "Korea, South",
-          city: "Gloucester"
-        },
-        {
-          name: "Doris Greene",
-          salary: "$63,542",
-          country: "Malawi",
-          city: "Feldkirchen in Kārnten"
-        },
-        {
-          name: "Mason Porter",
-          salary: "$78,615",
-          country: "Chile",
-          city: "Gloucester"
-        }
-      ]
     };
-  }
+  },
 };
 </script>
