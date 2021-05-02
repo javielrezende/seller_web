1f175e97160aa4d284b97325021b6a7c5550a6b4
Roger Rezende <javielrezende@gmail.com>
2021-05-02 16:44:49 -0300

    :sparkles: Add page to list sellers
    Add page to list sellers

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -6,25 +6,22 @@
       >
         <md-card>
           <md-card-header data-background-color="green">
-            <h4 class="title">Simple Table</h4>
-            <p class="category">Here is a subtitle for this table</p>
+            <h4 class="title">Vendedores cadastrados</h4>
+            <p class="category"></p>
           </md-card-header>
-          <md-card-content>
-            <simple-table table-header-color="green"></simple-table>
-          </md-card-content>
-        </md-card>
-      </div>
 
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
-      >
-        <md-card class="md-card-plain">
-          <md-card-header data-background-color="green">
-            <h4 class="title">Table on Plain Background</h4>
-            <p class="category">Here is a subtitle for this table</p>
-          </md-card-header>
+          <div class="md-layout-item md-size-100 text-left">
+            <md-button to="/seller" class="md-raised md-lightgreen"
+              >Cadastrar</md-button
+            >
+          </div>
+
           <md-card-content>
-            <ordered-table></ordered-table>
+            <simple-table
+              table-header-color="green"
+              :data="sellers"
+              :columns="columns"
+            ></simple-table>
           </md-card-content>
         </md-card>
       </div>
@@ -37,8 +34,38 @@ import { SimpleTable, OrderedTable } from "@/components";
 
 export default {
   components: {
-    OrderedTable,
-    SimpleTable
-  }
+    SimpleTable,
+  },
+  data() {
+    return {
+      sellers: [],
+      columns: [
+        { name: "name", label: "Nome" },
+        { name: "email", label: "Email" },
+        { name: "total_commission", label: "Comissão" },
+      ],
+    };
+  },
+  beforeMount() {
+    this.$http
+      .get("seller")
+      .then((response) => {
+        this.formatData(response.data.data);
+      })
+      .catch(function (error) {});
+  },
+  methods: {
+    formatData(data) {
+      const formatedData = [];
+
+      data.map((el) => {
+        if (!el.total_commission) el.total_commission = 0.0;
+        el.total_commission = "R$ " + el.total_commission;
+        formatedData.push(el);
+      });
+
+      this.sellers = formatedData;
+    },
+  },
 };
 </script>
