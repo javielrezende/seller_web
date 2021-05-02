1b43098c48b64305b3ab7fdaf0666f05577ae173
Roger Rezende <javielrezende@gmail.com>
2021-05-02 16:44:16 -0300

    :sparkles: Component to create a seller
    Component to create a seller

--- src/pages/UserProfile/EditProfileForm.vue
+++ src/pages/UserProfile/EditProfileForm.vue
@@ -2,74 +2,38 @@
   <form>
     <md-card>
       <md-card-header :data-background-color="dataBackgroundColor">
-        <h4 class="title">Edit Profile</h4>
-        <p class="category">Complete your profile</p>
+        <h4 class="title">Cadastrar vendedor</h4>
+        <p class="category"></p>
       </md-card-header>
 
       <md-card-content>
         <div class="md-layout">
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Company (disabled)</label>
-              <md-input v-model="disabled" disabled></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>User Name</label>
-              <md-input v-model="username" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Email Address</label>
-              <md-input v-model="emailadress" type="email"></md-input>
-            </md-field>
-          </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
             <md-field>
-              <label>First Name</label>
-              <md-input v-model="firstname" type="text"></md-input>
+              <label>Nome</label>
+              <md-input v-model="username" type="text" required></md-input>
             </md-field>
           </div>
+
           <div class="md-layout-item md-small-size-100 md-size-50">
             <md-field>
-              <label>Last Name</label>
-              <md-input v-model="lastname" type="text"></md-input>
+              <label>Email</label>
+              <md-input v-model="emailadress" type="email" required></md-input>
             </md-field>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-100">
-            <md-field>
-              <label>Adress</label>
-              <md-input v-model="address" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>City</label>
-              <md-input v-model="city" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Country</label>
-              <md-input v-model="country" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Postal Code</label>
-              <md-input v-model="code" type="number"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-size-100">
-            <md-field maxlength="5">
-              <label>About Me</label>
-              <md-textarea v-model="aboutme"></md-textarea>
-            </md-field>
-          </div>
+
           <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success">Update Profile</md-button>
+            <md-button
+              @click="submit"
+              class="md-raised md-success"
+              :disabled="
+                !username ||
+                username.length < 5 ||
+                !emailadress ||
+                emailadress.length < 5
+              "
+              >Cadastrar</md-button
+            >
           </div>
         </div>
       </md-card-content>
@@ -82,24 +46,54 @@ export default {
   props: {
     dataBackgroundColor: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
   data() {
     return {
       username: null,
-      disabled: null,
       emailadress: null,
-      lastname: null,
-      firstname: null,
-      address: null,
-      city: null,
-      country: null,
-      code: null,
-      aboutme:
-        "Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
     };
-  }
+  },
+  methods: {
+    submit() {
+      const form = new FormData();
+      form.append("name", this.username);
+      form.append("email", this.emailadress);
+
+      this.$http
+        .post("seller", form)
+        .then((response) => {
+          this.username = null;
+          this.emailadress = null;
+
+          this.$swal({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "success",
+            title: "Vendedor cadastrado com sucesso",
+          });
+          this.$router.push("/sellers");
+        })
+        .catch((error) => {
+          let errKey = Object.keys(error.response.data.errors)[0];
+          const messageError = JSON.parse(error.response.data.errors[errKey])
+            .message;
+          this.$swal({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: messageError,
+          });
+        });
+    },
+  },
 };
 </script>
 <style></style>
