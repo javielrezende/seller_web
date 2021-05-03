feaf492f56b9eeb442b51fe06a27edc51fcf8575
Roger Rezende <javielrezende@gmail.com>
2021-05-03 19:50:53 -0300

    ✨ Clean unused routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,27 +1,16 @@
 import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
 
-import Dashboard from "@/pages/Dashboard.vue";
 import SellerProfile from "@/pages/SellerProfile.vue";
 import Sale from "@/pages/Sale.vue";
 import SellersList from "@/pages/SellersList.vue";
 import SalesList from "@/pages/SalesList.vue";
-import Typography from "@/pages/Typography.vue";
-import Icons from "@/pages/Icons.vue";
-import Maps from "@/pages/Maps.vue";
-import Notifications from "@/pages/Notifications.vue";
-import UpgradeToPRO from "@/pages/UpgradeToPRO.vue";
 
 const routes = [
   {
     path: "/",
     component: DashboardLayout,
-    redirect: "/dashboard",
+    redirect: "/sellers",
     children: [
-      {
-        path: "dashboard",
-        name: "Dashboard",
-        component: Dashboard
-      },
       {
         path: "seller",
         name: "Seller Profile",
@@ -42,34 +31,6 @@ const routes = [
         name: "Vendas",
         component: SalesList
       },
-      {
-        path: "typography",
-        name: "Typography",
-        component: Typography
-      },
-      {
-        path: "icons",
-        name: "Icons",
-        component: Icons
-      },
-      {
-        path: "maps",
-        name: "Maps",
-        meta: {
-          hideFooter: true
-        },
-        component: Maps
-      },
-      {
-        path: "notifications",
-        name: "Notifications",
-        component: Notifications
-      },
-      {
-        path: "upgrade",
-        name: "Upgrade to PRO",
-        component: UpgradeToPRO
-      }
     ]
   }
 ];
